e927011dd87e0d0868fb7c5764fe31d2313693a9
Zhang Michael <michaelzhang@10-16-112-91.dynapool.wireless.nyu.edu>
2019-03-06 12:14:19 -0500

    michael

--- src/app.js
+++ src/app.js
@@ -1,8 +1,44 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
-app.set('view engine', 'hbs')
-app.use(express.urlencoded({extended:false}))
+app.set('view engine', 'hbs');
+app.use(express.urlencoded({extended:false}));
+
+const birdsdic = {birds:{"Bald Eagle":3,"Yellow Billed Duck":7,"Great Cormorant":4}};
+const filterNum = 0
+const birdslst = ["Bald Eagle","Yellow Billed Duck","Great Cormorant"];
+let birdstring = ``
+app.get('/', (req, res) => {
+    res.render('layout',null);
+ });
+
+ app.get('/birds', (req, res) => {
+     // name for the page 
+    let content;
+    if(req.ruery){
+        content = birdsdic;
+    }else{
+        let newdic = {birds:{}};
+        for(let i in birdsdic){
+            if(birdsdic[i] >= filterNum){
+                newdic.birds[i] = birdsdic.birds[i];
+            }
+        }
+        content = newdic;
+    }
+    res.render('home',content);
+ });
+
+app.get('/settings', (req, res) => {
+    if(req.query.hasOwnProperty('mininum')){
+    };
+});
 
 
+ app.post('/', (req, res) => {
+    res.send(req.body);
+    pets.push(req.body.name);
+    res.redirect('/');
+ });
 
+ app.listen(3000);
