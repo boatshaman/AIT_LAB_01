0064eee1f666c3963b8188d9c30c8fe4f1e5fb0c
Glitch (boatshaman-ait-lab-01) <none>
2019-03-06 18:21:33 +0000

    🍼👚 Updated with Glitch_02

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'hbs');
+app.set('view options', { layout: '../public/views/layout.hbs' });
 app.use(express.urlencoded({extended:false}));
 
 const birds = {"Bald Eagle":3,"Yellow Billed Duck":7,"Great Cormorant":4};
@@ -10,37 +11,37 @@ const filterNum = 0
 
 
 app.get('/', (req, res) => {
-    res.render('home',{});
+    res.render('../public/views/home',{});
  });
 
- app.get('/birds', (req, res) => {
-     // name for the page 
-    let content;
-    if(req.ruery){
-        content = birdsdic;
-    }else{
-        let newdic = {birds:{}};
-        for(let i in birdsdic){
-            if(birdsdic[i] >= filterNum){
-                newdic.birds[i] = birdsdic.birds[i];
-            }
-        }
-        content = newdic;
-    }
-    res.render('home',content);
- });
+//  app.get('/birds', (req, res) => {
+//      // name for the page 
+//     let content;
+//     if(req.ruery){
+//         content = birdsdic;
+//     }else{
+//         let newdic = {birds:{}};
+//         for(let i in birdsdic){
+//             if(birdsdic[i] >= filterNum){
+//                 newdic.birds[i] = birdsdic.birds[i];
+//             }
+//         }
+//         content = newdic;
+//     }
+//     res.render('home',content);
+//  });
 
-app.get('/settings', (req, res) => {
-    if(req.query.hasOwnProperty('mininum')){
-    };
-});
+// app.get('/settings', (req, res) => {
+//     if(req.query.hasOwnProperty('mininum')){
+//     };
+// });
 
 
- app.post('/', (req, res) => {
-    res.send(req.body);
-    pets.push(req.body.name);
-    res.redirect('/');
- });
+//  app.post('/', (req, res) => {
+//     res.send(req.body);
+//     pets.push(req.body.name);
+//     res.redirect('/');
+//  });
 
 app.listen(process.env.PORT);
 setInterval(() => {
